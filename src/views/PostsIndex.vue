0ffafec33a022d6449cf969d5e59934758c30c19
rebecca <r.young1025@gmail.com>
2021-02-11 15:06:20 -0600

    initial commit and routing practice

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="posts-index">
+    <h1>All Posts</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <img :src="post.image" :alt="post.name" />
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      posts: [],
+    };
+  },
+  created: function() {
+    axios.get("/api/posts").then(response => {
+      this.posts = response.data;
+    });
+  },
+};
+</script>
