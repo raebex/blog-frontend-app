3e72a56f26e0b811b4da2b5426c65d8be9f290a8
rebecca <r.young1025@gmail.com>
2021-02-15 14:29:00 -0600

    Add destroy route

--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -4,6 +4,8 @@
     <img :src="post.image" :alt="post.title" />
     <p>{{ post.body }}</p>
     <router-link :to="`/posts/${post.id}/edit`">Edit</router-link>
+    <button v-on:click="destroyPost()">Destroy</button>
+    {{ destroySuccess }}
   </div>
 </template>
 
@@ -13,12 +15,27 @@ export default {
   data: function() {
     return {
       post: {},
+      destroySuccess: "",
     };
   },
   created: function() {
     axios.get(`/api/posts/${this.$route.params.id}`).then(response => {
       this.post = response.data;
     });
+  },
+  methods: {
+    destroyPost: function() {
+      if (confirm("Are you sure you want to delete this post?")) {
+        axios.delete(`/api/posts/${this.post.id}`).then(response => {
+          console.log(response.data);
+          this.destroySuccess = response.data.message;
+          var self = this;
+          setTimeout(function() {
+            self.$router.push("/posts");
+          }, 1500);
+        });
+      }
+    },
   },
 };
 </script>
